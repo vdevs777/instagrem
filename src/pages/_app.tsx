4011e3c93008dc6cb57ec539ee84efe4550f2a60
unknown <jeivi.777@gmail.com>
2024-03-10 13:56:49 -0300

    tada: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import { queryClient } from "@/lib/queryClient";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
